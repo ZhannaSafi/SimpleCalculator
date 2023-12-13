ae33d634137c4f3e2515d0616bd3adc5cf9eb567
ZhannaSafi <144216823+ZhannaSafi@users.noreply.github.com>
2023-12-13 17:07:09 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Введите второе число: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Введите математический оператор (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result = 0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Ошибка: Деление на ноль недопустимо.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Ошибка: Недопустимый оператор");
+                return;
+        }
+
+        System.out.println("Результат: " + result);
+    }
+}
